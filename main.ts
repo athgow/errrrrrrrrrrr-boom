b703ff70f23e242a7fc4a3697ac8dee2aa9e0e67
athgow <69103862+athgow@users.noreply.github.com>
2020-09-27 10:47:32 +0200

    just added music

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+music.setVolume(255)
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 10) {
+        maqueen.motorStop(maqueen.Motors.All)
+        music.playMelody("C D C D E F E D ", 120)
+        basic.pause(1000)
+    }
+    basic.pause(200)
+})
